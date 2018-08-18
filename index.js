01c7a599ac6ad01219f0be45dfca7cfcd5182be2
roelver <roel@romaniflo.nl>
2018-08-18 12:24:28 +0200

    between 0 - 100

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    const num = Math.ceil(Math.random()*1000);
+    const num = Math.ceil(Math.random()*100);
     const reply = { value: num};
     res.setHeader("Content-Type", "application/json");
     res.send(JSON.stringify(reply));    
